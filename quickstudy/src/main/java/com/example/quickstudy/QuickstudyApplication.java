1f4466fe96e75cb349fa32e24c00e04e76897f4c
daniel-z-johnson <danzaljohnson@gmail.com>
2026-02-16 20:21:21 -0600

    first commit

---
+++ quickstudy/src/main/java/com/example/quickstudy/QuickstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstudyApplication.class, args);
+	}
+
+}
---
+++ quickstudy/src/test/java/com/example/quickstudy/QuickstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
